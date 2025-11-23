5c508e32f249154fe137418c6fe6d3c02e4f7b8b
Arllain <arllain@gmail.com>
2025-11-23 20:16:05 -0300

    Testando validações com testes unitários

--- src/test/java/com/arllain/algashop/ordering/domain/entity/CustomerTest.java
+++ src/test/java/com/arllain/algashop/ordering/domain/entity/CustomerTest.java
@@ -1,26 +1,47 @@
 package com.arllain.algashop.ordering.domain.entity;
 
 import com.arllain.algashop.ordering.domain.utility.IdGenerator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 
 class CustomerTest {
 
     @Test
-    public void testingCustomer() {
+    void given_invalidEmail_whenTryCreateCustomer_shouldGenerateException() {
 
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            new Customer(
+                    IdGenerator.generateTimeBasedUUID(),
+                    "John Doe",
+                    LocalDate.of(1990, 6, 11),
+                    "invalid",
+                    "478-256-2504",
+                    "255-08-2504",
+                    false,
+                    OffsetDateTime.now()
+            );
+        });
+    }
+
+
+    @Test
+    void given_invalidEmail_whenTryUpdateCustomerEmail_shouldGenerateException() {
         Customer customer = new Customer(
-                IdGenerator.generateTimeBasedUUID(),
-                "Arllain Silva",
-                LocalDate.of(1976,12,18),
-                "email@email.com",
-                "123487",
-                "123456789",
-                true,
+            IdGenerator.generateTimeBasedUUID(),
+                "John Doe",
+                LocalDate.of(1990, 6, 11),
+                "john.doe@gmail.com",
+                "478-256-2504",
+                "255-08-2504",
+                false,
                 OffsetDateTime.now()
-        );
+            );
 
-        customer.addLoyaltyPoints(10);
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            customer.changeEmail("invalid");
+        });
     }
 }
