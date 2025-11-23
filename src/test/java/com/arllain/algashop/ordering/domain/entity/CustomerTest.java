1586250bd0aa8d8d2e8ad3532af711a3220aab9d
Arllain <arllain@gmail.com>
2025-11-23 18:41:38 -0300

    Refatorando a Customer Entity em direção a um Rich model

--- src/test/java/com/arllain/algashop/ordering/domain/entity/CustomerTest.java
+++ src/test/java/com/arllain/algashop/ordering/domain/entity/CustomerTest.java
@@ -1,6 +1,9 @@
 package com.arllain.algashop.ordering.domain.entity;
 
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 class CustomerTest {
@@ -8,10 +11,18 @@ class CustomerTest {
     @Test
     public void testingCustomer() {
 
-        Customer customer = new Customer();
-        customer.setId(UUID.randomUUID());
-        customer.setFullName("Arllain Silva");
-        customer.setDocument("123456");
-        customer.setLoyaltyPoints(10);
+        Customer customer = new Customer(
+                UUID.randomUUID(),
+                "Arllain Silva",
+                LocalDate.of(1976,12,18),
+                "email@email.com",
+                "123487",
+                "123456789",
+                true,
+                OffsetDateTime.now()
+        );
+
+        customer.addLoyaltyPoints(10);
+        customer.changeEmail();
     }
 }
